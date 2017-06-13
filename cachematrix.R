c90c9b2af6028fcc92e5202ad5988364c78a1036
Santiago Ramirez G <santi_ramirez04@yahoo.com>
2017-06-13 16:23:29 +0200

    include comments for second function and also indications for validating de functions

--- cachematrix.R
+++ cachematrix.R
@@ -11,6 +11,8 @@
 ## to write in an object savd in the Parent environment "m" (Which will cache (store) the Inverse every time)
 ## a new matrix is set
 
+##NOTE: See **below** some indications for testing this functions
+
 
 ## Write a short comment describing this function
 ## Creates an Object Type "MakecacheMatrix" that is required to call the second function "cacheSolve"
@@ -37,6 +39,11 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 ## Write a short comment describing this function
+## This function is directly connected with makeCacheMatrix
+## It receives an object returned by makeCacheMatrix as a parameter
+## This makes possible to use the Getters and setters as defined in the previous function
+## when the value of "m" our global variable is not set("Is NULL"), the inverse of the Matrix
+## is calculated, otherwise the value will be retrieved from cache and the "m" malue will be returned
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
@@ -52,3 +59,31 @@ cacheSolve <- function(x, ...) {
         x$setinverse(m)
         m
 }
+
+
+
+# m1 <- matrix(c(1/2, -1/4, -1, 3/4), nrow = 2, ncol = 2)
+# m2 <- matrix(c(4, 2, 7, 6), nrow = 2, ncol = 2)
+#
+# Validate_m1_inv <- solve(m1)
+# Validate_m2_inv <- solve(m2)
+#
+# aMatrix <- makeCacheMatrix(m1) ## Creates an Object type "MakeCacheMatrix" to be used as a paremeter when calling "cachesolve"
+# aMatrix$get()               # retrieve the value of x. Since the makeCacheMatrix function was used, the return value will be x, that has the same Matrix given as parameter
+# aMatrix$getinverse()           # retrieve the value of m, which is NULL because there is no (untill this point) inverse value stored in m.
+# aMatrix$set(m1)          # set the matrix to the value of m1 to get its inverse (already confirmed it is a square invertible matrix)
+# cacheSolve(aMatrix)          # Inverse is calculated from m1 for the first time. The answer (the inverse) is stored in "m". "m" is not NULL anymore
+# aMatrix$getinverse()           # retrieve it directly, now that it has been cached
+#
+# ## Validate Function return match with the previously INV calculated (Only for validating purposes)
+#
+# identical(aMatrix$getinverse(), Validate_m1_inv)	## Validate if 2 matrix are identical. Answer is TRUE
+#
+# aMatrix$set(m2)         ## Set a new Matrix, so the inverse will ve calculated from this matrix
+# aMatrix$getinverse()     ## Returns again NULL because the Matrix has changed. A new Inverse has to be calculated (for this Matrix) to be ables to get it ffrom Cache
+# cacheSolve(aMatrix)	## Inverse of new Matrix (m2) is calculated
+# aMatrix$getinverse()	## Get the inverse value from Cache. Since the value was already calculated, there is an answer
+#
+# ## Validate Function return match with the previously INV calculated (Only for validating purposes)
+#
+# identical(aMatrix$getinverse(), Validate_m2_inv)	## Validate if 2 matrix are identical. Answer is TRUE
